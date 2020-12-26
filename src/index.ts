1478cb7ea60d0c426cc8e878a82e6f6b0b57cad9
Marco <marco.mt19@gmail.com>
2020-12-26 17:16:01 +0100

    a simple test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-import Koa = require('koa');
-import Router = require('@koa/router');
+// import Koa = require('koa');
+// import Router = require('@koa/router');
 
-const app = new Koa();
-const router = new Router();
+// const app = new Koa();
+// const router = new Router();
 
-router.get('/', async (ctx, next) => {
-  ctx.body = 'Hello world!';
-  await next();
-});
+// router.get('/', async (ctx, next) => {
+//   ctx.body = 'Hello world!';
+//   await next();
+// });
 
-app.use(router.routes());
+// app.use(router.routes());
 
-app.listen(3000);
+// app.listen(3000);
+
+class Flamingo {
+  constructor() {
+    console.log('Hello World')!;
+  }
+}
+const flamingo = new Flamingo();
+export {flamingo};
